6e398a67c81551aab09f1f4cdac6cbfd6fed5adf
amex23 <janamirxavier@gmail.com>
2026-03-28 20:06:24 +0800

    link added

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -210,7 +210,7 @@ export default function Welcome({
                     <div className="text-md md:text-xl text-center md:text-start flex gap-1 items-center"><b>20%</b> of Profit goes to <a href="https://www.worldvision.org.ph/"><b>WorldVision</b></a> Charity <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="#37B6FF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hand-coins-icon lucide-hand-coins"><path d="M11 15h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 17"/><path d="m7 21 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9"/><path d="m2 16 6 6"/><circle cx="16" cy="9" r="2.9"/><circle cx="6" cy="5" r="3"/></svg></div>
                   
                           <div className="mt-3 flex gap-3 md:mt-0 block text-center">
-                            ⓒ Copyright 2026 | <a href="">Contact Us</a> | Made with ❤️
+                            ⓒ Copyright 2026 | <a href="/contact-us">Contact Us</a> | Made with ❤️
                            </div>  
                  
                     
