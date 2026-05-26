8144d2a99237ef16294c085e0c8ae3f7ca8eecb6
jantinrx23 <Janamir@tinrx.com>
2026-05-26 16:44:43 +0800

    cancel subscription

--- resources/js/pages/Products/Components/UserView.tsx
+++ resources/js/pages/Products/Components/UserView.tsx
@@ -187,7 +187,7 @@ export default function UserView({ products, myProfile, editUrlBase }: UserViewP
                                                 </span>
                                                 <a href="/subscribe/cancel" onClick={handleCancel}>
                                                     <Button size="sm" variant="outline" className="text-red-500 border-red-200 hover:bg-red-50">
-                                                        Cancel
+                                                        Cancel Subscription
                                                     </Button>
                                                 </a>
                                             </>
