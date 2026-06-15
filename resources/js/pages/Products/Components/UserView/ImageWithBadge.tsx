a6088a22ef1077ddd7bc5bd412b7a2757a28863c
jantinrx23 <Janamir@tinrx.com>
2026-06-15 23:51:22 +0800

    eye icon

--- resources/js/pages/Products/Components/UserView/ImageWithBadge.tsx
+++ resources/js/pages/Products/Components/UserView/ImageWithBadge.tsx
@@ -8,7 +8,7 @@ export default function ImageWithBadge({ src, alt, viewCount, size = 'sm' }: { s
         <div className="relative">
             <img src={src} alt={alt} className={`${cls} object-cover rounded border shadow-sm`} />
 
-            <span className="absolute top-[3%] z-[99] flex bg-[#4A4A4A] flex items-center justify-center rounded-xs ml-2 mt-2 px-[1px] py-[1px]">
+            <span className="absolute top-[3%] z-[9] flex bg-[#4A4A4A] flex items-center justify-center rounded-xs ml-2 mt-2 px-[1px] py-[1px]">
                 <span className="px-1 py-0 text-xs text-white font-bold rounded-sm leading-tight">
                         <FontAwesomeIcon icon={faEye} />
                 </span>
